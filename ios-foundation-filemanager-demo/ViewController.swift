05218871d9d5caa5b3bea28394d4f7babef79f0c
stv-ekushida <e.kushida@st-ventures.jp>
2017-04-07 16:17:18 +0900

    Initial Commit

---
+++ ios-foundation-filemanager-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-foundation-filemanager-demo
+//
+//  Created by Eiji Kushida on 2017/04/07.
+//  Copyright © 2017年 Eiji Kushida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
